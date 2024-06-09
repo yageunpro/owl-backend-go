29307a9da859c51bb44ddf601e56104714681886
GyeongTaek Kim (mansuiki) <mansuiki@gmail.com>
2024-06-09 23:42:45 +0900

    chore: Change google calendar logic

--- internal/google/calendar/calendar.go
+++ internal/google/calendar/calendar.go
@@ -33,23 +33,34 @@ func GetAllEvents(ctx context.Context, arg Param) (*Result, error) {
 		return nil, errors.Join(errors.New("failed to init calendar client"), err)
 	}
 
-	qry := srv.Events.List("primary").
-		TimeMin(time.Now().UTC().Format(time.RFC3339))
+	krTimeZone := time.FixedZone("KR", 9*60*60)
+	qry := srv.Events.List("primary").TimeZone("Asia/Seoul")
 
 	res := Result{
 		SyncToken: "",
 		Events:    make([]Event, 0),
 	}
 
+	useSyncToken := false
 	pageToken := ""
+	if arg.SyncToken != "" {
+		useSyncToken = true
+	}
 
 	for {
 		q := qry
+		if useSyncToken {
+			q = qry.SyncToken(arg.SyncToken)
+		}
 		if pageToken != "" {
 			q = qry.PageToken(pageToken)
 		}
 		events, err := q.Do()
 		if err != nil {
+			if useSyncToken {
+				useSyncToken = false
+				continue
+			}
 			return nil, errors.Join(errors.New("failed to list events"), err)
 		}
 
@@ -60,17 +71,22 @@ func GetAllEvents(ctx context.Context, arg Param) (*Result, error) {
 
 			if events.Items[i].Start == nil || events.Items[i].End == nil {
 				slog.Error("failed to parse time, nil", slog.String("title", events.Items[i].Summary))
-				continue
 			}
 			startTime, err = time.Parse(time.RFC3339, events.Items[i].Start.DateTime)
 			if err != nil {
-				slog.Error("failed to parse start time", slog.String("event", events.Items[i].Start.DateTime))
-				continue
+				startTime, err = time.ParseInLocation(time.DateOnly, events.Items[i].Start.Date, krTimeZone)
+				if err != nil {
+					slog.Error("failed to parse start time", slog.String("event", events.Items[i].Start.DateTime))
+					continue
+				}
 			}
 			endTime, err = time.Parse(time.RFC3339, events.Items[i].End.DateTime)
 			if err != nil {
-				slog.Error("failed to parse end time", slog.String("event", events.Items[i].Start.DateTime))
-				continue
+				endTime, err = time.ParseInLocation(time.DateOnly, events.Items[i].End.Date, krTimeZone)
+				if err != nil {
+					slog.Error("failed to parse end time", slog.String("event", events.Items[i].Start.DateTime))
+					continue
+				}
 			}
 
 			res.Events = append(res.Events, Event{
